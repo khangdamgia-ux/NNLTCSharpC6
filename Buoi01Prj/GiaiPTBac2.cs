4d4cca76ad3100c5a0e84ff520798405b48c6ca2
khangdamgia-ux <khangdamgia@gmail.com>
2026-09-17 21:13:53 +0700

    Initial commit

---
+++ Buoi01Prj/GiaiPTBac2.cs
@@ -0,0 +1,13 @@
+﻿using MyLib;
+namespace Buoi01Prj;
+public class GiaiPTBac2
+{
+public static void Main(string[] args)
+{
+double x1 = 0, x2 = 0;
+int sn = LibBaiTap.GiaiPTBac2(1, -3, 2, ref x1, ref x2);
+Console.WriteLine($"So nghiem: {sn}");
+Console.WriteLine($"x1 = {x1}");
+Console.WriteLine($"x2 = {x2}");
+}
+}
